e590625dacc78dc110493ec96f5353ce52bf1377
lzydavid <nodotanolife@hotmail.com>
2023-02-07 10:03:33 +0800

    init

---
+++ src/main/java/SSF_2022/day12_ws/Day12WsApplication.java
@@ -0,0 +1,13 @@
+package SSF_2022.day12_ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12WsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SSF_2022/day12_ws/Day12WsApplicationTests.java
@@ -0,0 +1,13 @@
+package SSF_2022.day12_ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
